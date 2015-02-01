96f1b32bdafa7b182159aaf300ba826a2c116329
Ryuichi YAMAMOTO <zryuichi@gmail.com>
2015-02-01 21:30:09 +0900

    pep8

--- pystav/feature.py
+++ pystav/feature.py
@@ -3,6 +3,7 @@
 
 import numpy as np
 
+
 def append_delta(src):
     """
     Append delta feature to static feature vector.
@@ -17,16 +18,16 @@ def append_delta(src):
     a sequence of joint static and delta feature vector
 
     """
-    new_feature = np.empty((src.shape[0], src.shape[1]*2))
-    
+    new_feature = np.empty((src.shape[0], src.shape[1] * 2))
+
     T, dim = src.shape
     for t in range(T):
-        ff = np.zeros(2*dim)
+        ff = np.zeros(2 * dim)
         ff[0:dim] = src[t][:dim]
-        if t-1 >= 0:
-            ff[dim:] -= 0.5*src[t-1][:dim]
-        if t+1 < T:
-            ff[dim:] += 0.5*src[t+1][:dim]
+        if t - 1 >= 0:
+            ff[dim:] -= 0.5 * src[t - 1][:dim]
+        if t + 1 < T:
+            ff[dim:] += 0.5 * src[t + 1][:dim]
 
         new_feature[t] = ff
 
--- pystav/gmmmap.py
+++ pystav/gmmmap.py
@@ -8,13 +8,15 @@ import scipy.linalg
 import scipy.sparse
 import scipy.sparse.linalg
 
+
 class GMMMap:
-    """GMM-based frame-by-frame speech parameter mapping. 
+
+    """GMM-based frame-by-frame speech parameter mapping.
 
     GMMMap represents a class to transform spectral features of a source
     speaker to that of a target speaker based on Gaussian Mixture Models
     of source and target joint spectral features.
-    
+
     Notation
     --------
     Source speaker's feature: X = {x_t}, 0 <= t < T
@@ -23,13 +25,13 @@ class GMMMap:
 
     Parameters
     ----------
-    gmm : sklean.mixture.GMM
+    gmm : sklearn.mixture.GMM
         Gaussian Mixture Models of source and target joint features
-    
+
     swap : bool
         True: source -> target
         False target -> source
-    
+
     Attributes
     ----------
     num_mixtures : int
@@ -44,27 +46,27 @@ class GMMMap:
     tgt_means : array, shape (`num_mixtures`, `order of spectral feature`)
         means of GMM for a target speaker
 
-    covarXX : array, shape (`num_mixtures`, `order of spectral feature`, 
+    covarXX : array, shape (`num_mixtures`, `order of spectral feature`,
         `order of spectral feature`)
         variance matrix of source speaker's spectral feature
 
-    covarXY : array, shape (`num_mixtures`, `order of spectral feature`, 
+    covarXY : array, shape (`num_mixtures`, `order of spectral feature`,
         `order of spectral feature`)
         covariance matrix of source and target speaker's spectral feature
 
-    covarYX : array, shape (`num_mixtures`, `order of spectral feature`, 
+    covarYX : array, shape (`num_mixtures`, `order of spectral feature`,
         `order of spectral feature`)
         covariance matrix of target and source speaker's spectral feature
 
-    covarYY : array, shape (`num_mixtures`, `order of spectral feature`, 
+    covarYY : array, shape (`num_mixtures`, `order of spectral feature`,
         `order of spectral feature`)
         variance matrix of target speaker's spectral feature
-    
-    D : array, shape (`num_mixtures`, `order of spectral feature`, 
+
+    D : array, shape (`num_mixtures`, `order of spectral feature`,
         `order of spectral feature`)
         covariance matrices of target static spectral features
 
-    px : sklean.mixture.GMM
+    px : sklearn.mixture.GMM
         Gaussian Mixture Models of source speaker's features
 
     Reference
@@ -74,9 +76,10 @@ class GMMMap:
         http://isw3.naist.jp/~tomoki/Tomoki/Journals/IEEE-Nov-2007_MLVC.pdf
 
     """
+
     def __init__(self, gmm, swap=False):
         # D is the order of spectral feature for a speaker
-        self.num_mixtures, D = gmm.means_.shape[0], gmm.means_.shape[1]/2
+        self.num_mixtures, D = gmm.means_.shape[0], gmm.means_.shape[1] / 2
         self.weights = gmm.weights_
 
         # Split source and target parameters from joint GMM
@@ -94,7 +97,8 @@ class GMMMap:
             self.covarYX, self.covarXY = self.XY, self.covarYX
 
         # Compute D eq.(12) in [Toda 2007]
-        self.D = np.zeros(self.num_mixtures*D*D).reshape(self.num_mixtures, D, D)
+        self.D = np.zeros(
+            self.num_mixtures * D * D).reshape(self.num_mixtures, D, D)
         for m in range(self.num_mixtures):
             xx_inv_xy = np.linalg.solve(self.covarXX[m], self.covarXY[m])
             self.D[m] = self.covarYY[m] - np.dot(self.covarYX[m], xx_inv_xy)
@@ -108,7 +112,7 @@ class GMMMap:
 
     def convert(self, src):
         """
-        Mapping source spectral feature x to target spectral feature y 
+        Mapping source spectral feature x to target spectral feature y
         so that minimize the mean least squared error.
         More specifically, it returns the value E(p(y|x)].
 
@@ -128,34 +132,36 @@ class GMMMap:
         for m in range(self.num_mixtures):
             xx = np.linalg.solve(self.covarXX[m], src - self.src_means[m])
             E[m] = self.tgt_means[m] + self.covarYX[m].dot(xx)
-                
+
         # Eq.(9) p(m|x)
         posterior = self.px.predict_proba(np.atleast_2d(src))
 
         # Eq.(13) conditinal mean E[p(y|x)]
         return posterior.dot(E)
-            
+
+
 class TrajectoryGMMMap(GMMMap):
+
     """
     Trajectory-based speech parameter mapping for voice conversion
     based on the maximum likelihood criterion.
 
     Parameters
     ----------
-    gmm : sklean.mixture.GMM
+    gmm : sklearn.mixture.GMM
         Gaussian Mixture Models of source and target speaker joint features
 
-    gv : sklean.mixture.GMM (default=None)
+    gv : sklearn.mixture.GMM (default=None)
         Gaussian Mixture Models of target speaker's global variance of spectral
         feature
-    
+
     swap : bool (default=False)
         True: source -> target
         False target -> source
 
     Attributes
     ----------
-    TODO 
+    TODO
 
     Reference
     ---------
@@ -163,6 +169,7 @@ class TrajectoryGMMMap(GMMMap):
         of Spectral Parameter Trajectory.
         http://isw3.naist.jp/~tomoki/Tomoki/Journals/IEEE-Nov-2007_MLVC.pdf
     """
+
     def __init__(self, gmm, T, gv=None, swap=False):
         GMMMap.__init__(self, gmm, swap)
 
@@ -170,10 +177,10 @@ class TrajectoryGMMMap(GMMMap):
         # shape[1] = d(src) + d(src_delta) + d(tgt) + d(tgt_delta)
         D = gmm.means_.shape[1] / 4
 
-        ## Setup for Trajectory-based mapping
+        # Setup for Trajectory-based mapping
         self.__construct_weight_matrix(T, D)
 
-        ## Setup for GV post-filtering
+        # Setup for GV post-filtering
         # It is assumed that GV is modeled as a single mixture GMM
         if gv != None:
             self.gv_mean = gv.means_[0]
@@ -184,18 +191,18 @@ class TrajectoryGMMMap(GMMMap):
         # Construct Weight matrix W
         # Eq.(25) ~ (28)
         for t in range(T):
-            w0 = scipy.sparse.lil_matrix((D, D*T))
-            w1 = scipy.sparse.lil_matrix((D, D*T))
-            w0[0:,t*D:(t+1)*D] = scipy.sparse.diags(np.ones(D), 0)
+            w0 = scipy.sparse.lil_matrix((D, D * T))
+            w1 = scipy.sparse.lil_matrix((D, D * T))
+            w0[0:, t * D:(t + 1) * D] = scipy.sparse.diags(np.ones(D), 0)
 
-            if t-1 >= 0:
+            if t - 1 >= 0:
                 tmp = np.zeros(D)
                 tmp.fill(-0.5)
-                w1[0:,(t-1)*D:t*D] = scipy.sparse.diags(tmp, 0)
-            if t+1 < T:
+                w1[0:, (t - 1) * D:t * D] = scipy.sparse.diags(tmp, 0)
+            if t + 1 < T:
                 tmp = np.zeros(D)
                 tmp.fill(0.5)
-                w1[0:,(t+1)*D:(t+2)*D] = scipy.sparse.diags(tmp, 0)
+                w1[0:, (t + 1) * D:(t + 2) * D] = scipy.sparse.diags(tmp, 0)
 
             W_t = scipy.sparse.vstack([w0, w1])
 
@@ -209,11 +216,11 @@ class TrajectoryGMMMap(GMMMap):
 
         self.W = scipy.sparse.csr_matrix(self.W)
 
-        assert self.W.shape == (2*D*T, D*T)
-        
+        assert self.W.shape == (2 * D * T, D * T)
+
     def convert(self, src):
         """
-        Mapping source spectral feature x to target spectral feature y 
+        Mapping source spectral feature x to target spectral feature y
         so that maximize the likelihood of y given x.
 
         Parameters
@@ -226,7 +233,7 @@ class TrajectoryGMMMap(GMMMap):
         ------
         a sequence of transformed spectral features
         """
-        T, D = src.shape[0], src.shape[1]/2
+        T, D = src.shape[0], src.shape[1] / 2
 
         if T != self.T:
             self.__construct_weight_matrix(T, D)
@@ -235,16 +242,16 @@ class TrajectoryGMMMap(GMMMap):
         optimum_mix = self.px.predict(src)
 
         # Compute E eq.(40)
-        self.E = np.zeros((T, 2*D))
+        self.E = np.zeros((T, 2 * D))
         for t in range(T):
-            m = optimum_mix[t] # estimated mixture index at time t
+            m = optimum_mix[t]  # estimated mixture index at time t
             xx = np.linalg.solve(self.covarXX[m], src[t] - self.src_means[m])
             # Eq. (22)
             self.E[t] = self.tgt_means[m] + np.dot(self.covarYX[m], xx)
         self.E = self.E.flatten()
 
         # Compute D eq.(41). Note that self.D represents D^-1.
-        self.D = np.zeros((T, 2*D, 2*D))
+        self.D = np.zeros((T, 2 * D, 2 * D))
         for t in range(T):
             m = optimum_mix[t]
             xx_inv_xy = np.linalg.solve(self.covarXX[m], self.covarXY[m])
@@ -256,13 +263,13 @@ class TrajectoryGMMMap(GMMMap):
         # Compute target static features
         # eq.(39)
         covar = self.W.T.dot(self.D.dot(self.W))
-        y = scipy.sparse.linalg.spsolve(covar, self.W.T.dot(self.D.dot(self.E)),\
+        y = scipy.sparse.linalg.spsolve(covar, self.W.T.dot(self.D.dot(self.E)),
                                         use_umfpack=False)
         return y.reshape((T, D))
 
     def convert_with_gv(self, src, epoches=1000, learning_rate=0.0045):
         """
-        Mapping source spectral feature x to target spectral feature y 
+        Mapping source spectral feature x to target spectral feature y
         so that maximize the likelihood of y given x with considering
         global variance.
 
@@ -283,14 +290,14 @@ class TrajectoryGMMMap(GMMMap):
         y = self.convert(src)
 
         T, D = y.shape
-        omega = 1.0/(2.0*T)
+        omega = 1.0 / (2.0 * T)
 
         # Gradient decent
         for i in range(epoches):
             # Eq.(57)
             grad_y = -self.W.T.dot(self.D.dot(self.W.dot(y.flatten()))) \
-                     + self.W.T.dot(self.D.dot(self.E))
-            grad = omega*grad_y + self.__gv_grad(y).flatten()
+                + self.W.T.dot(self.D.dot(self.E))
+            grad = omega * grad_y + self.__gv_grad(y).flatten()
             print i, np.linalg.norm(grad)
             y = y + learning_rate * grad.reshape((T, D))
 
@@ -310,13 +317,14 @@ class TrajectoryGMMMap(GMMMap):
         gradient of the likelihood with regard to global variance
         """
         T, D = y.shape
-        
-        gv = np.var(y, axis=0) # global variance over time
+
+        gv = np.var(y, axis=0)  # global variance over time
         y_mean = np.mean(y, axis=0)
         v = np.zeros((T, D))
 
         # Eq.(55)
         for t in range(T):
-            v[t] = -2.0/T*np.dot(self.Pv.T, (gv - self.gv_mean))*(y[t] - y_mean)
+            v[t] = -2.0 / T * \
+                np.dot(self.Pv.T, (gv - self.gv_mean)) * (y[t] - y_mean)
 
         return v
